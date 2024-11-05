df4b3cabc4526a0c39b67365f9b068a6ae152742
AlexEnersen <alex.enersen@huskers.unl.edu>
2024-11-04 21:10:42 -0600

    reqs

--- VTAPS_v1/settings.py
+++ VTAPS_v1/settings.py
@@ -57,7 +57,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['129.93.1.31', 'localhost', ".awsapprunner.com", "vtaps.org"]
+ALLOWED_HOSTS = ['129.93.1.31', 'localhost', ".awsapprunner.com", "vtaps.org/"]
 
 
 # Application definition
