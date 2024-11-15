07de29fe6aed7410e26b7efa848b869b585a7a7b
AlexEnersen <alex.enersen@huskers.unl.edu>
2024-11-15 13:24:30 -0600

    pyscopg2

--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ from django.contrib.sessions.models import Session
 
 def home(response):
     # response.session.clear()
+    print('HOME IS HERE!!!!')
     Session.objects.all().delete()
     user_id = response.session.get("user_id", None)
     if not user_id == None:
