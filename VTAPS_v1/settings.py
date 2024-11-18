37b1eea73023338fa2644f9646c1338947d8788d
AlexEnersen <alex.enersen@huskers.unl.edu>
2024-11-17 18:11:29 -0600

    csrf

--- VTAPS_v1/settings.py
+++ VTAPS_v1/settings.py
@@ -52,7 +52,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['129.93.1.31', 'localhost', ".awsapprunner.com", "vtaps.org"]
 
