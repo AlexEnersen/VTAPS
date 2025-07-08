7b2884aed03887e1f013ae4dec14ab2710d586ef
AlexEnersen <alex.enersen@huskers.unl.edu>
2025-07-08 09:41:28 -0500

    Added User

--- VTAPS/asgi.py
+++ VTAPS/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VTAPS_v1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VTAPS.settings')
 
 application = get_asgi_application()
