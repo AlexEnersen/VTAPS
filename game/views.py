fb23ef0aef5fce1486d359c7d3380779d2fe7781
AlexEnersen <alex.enersen@huskers.unl.edu>
2025-09-24 22:56:13 -0500

    Added cultivars

--- game/views.py
+++ game/views.py
@@ -99,8 +99,8 @@ def runGame(request, game_id=None):
             context['fert_form'] = fert_form
             return render(request, "game/init.html", context)
         else:
-            # if gameProfile.week < 22 and not gameProfile.finished:              ##### NORMAL MODE
-            if gameProfile.week <= 1 and not gameProfile.finished:           ##### FINAL PAGE    DEBUG MODE
+            if gameProfile.week < 22 and not gameProfile.finished:              ##### NORMAL MODE
+            # if gameProfile.week <= 1 and not gameProfile.finished:            ##### FINAL PAGE    DEBUG MODE
                 context = weeklySelection(request, gameProfile)
                 if context is None:
                     return redirect(game_url)
