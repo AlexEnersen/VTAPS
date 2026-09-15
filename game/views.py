786229e20bf716603597ed1066f2c0001b269d1d
AlexEnersen <alex.enersen@huskers.unl.edu>
2026-09-15 11:33:31 -0500

    Added hybrid check

--- game/views.py
+++ game/views.py
@@ -1286,10 +1286,14 @@ def getNitrogenUptake(date, gameOutputs):
     return totalNitrogenUptake
 
 def computeDSSAT(hybrid, gameInputs, gamePath): 
+
+    if hybrid == None:
+        hybrid = "IB2074 Channel213-19VTPRI"
     
     uploadInputs(gameInputs, gamePath) 
     zip_buffer = io.BytesIO()
 
+    hybrid = None
     subHybrid = list(filter(None, hybrid.split(" ")))[0]
     commandString = "../../DSCSM048 %s A %s" % (subHybrid, gameInputs['MZX_name'])
 
